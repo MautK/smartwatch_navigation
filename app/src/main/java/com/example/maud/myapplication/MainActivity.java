f21a9da9ff40071eb00207a64a3a306f93206a52
g.luensdorf <g.luensdorf@gmail.com>
2018-06-08 10:59:43 +0200

    test getLocation

--- app/src/main/java/com/example/maud/myapplication/MainActivity.java
+++ app/src/main/java/com/example/maud/myapplication/MainActivity.java
@@ -2,19 +2,28 @@ package com.example.maud.myapplication;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
+    private LocationManager mLocationManager;
+    private String TAG = "FOOBAR";
+    private Location mLocation = new Location("");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +31,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         checkPermission(this);
 
+        mLocation.setLatitude(0.0d);
+        mLocation.setLongitude(0.0d);
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1,1, mLocationListener);
 
-//        final Button getCoordsButton = findViewById(R.id.coords_button);
-//
-//        getCoordsButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                checkPermission();
-//            }
-//        });
+        final Button getCoordsButton = findViewById(R.id.coords_button);
+
+
+        getCoordsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = mLocation.toString();
+                int duration = Toast.LENGTH_SHORT;
+
+                Log.d(TAG, "onClick: " + mLocation.toString());
+                Toast.makeText(context, text, duration).show();
+            }
+        });
 
     }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            mLocation = location;
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     public void checkPermission(Activity thisActivity) {
         if (ContextCompat.checkSelfPermission(thisActivity,
@@ -43,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             );
         } else {
             //what to take instead of mMap?
-            mMap.setMyLocationEnabled(true);
+            //mMap.setMyLocationEnabled(true);
         }
     }
 
